36c32781bc562dd4d5e284a003118f2cc26d1b22
papo84 <papo84@gmail.com>
2018-06-16 00:34:14 +0300

    removed test code and print; added some comments

--- src/invokers/scrapArticleInvoker.js
+++ src/invokers/scrapArticleInvoker.js
@@ -2,6 +2,7 @@ const rp = require('request-promise');
 const cheerio = require('cheerio');
 const db = require('../db/mongoClient');
 let count = 0;
+//  Array of hebrew months used to parse dates from hebrew to Datetime 
 const hebrewMonths = [
     'ינו',
     'פבר',
@@ -33,6 +34,8 @@ function saveAllCommentsFromURL({ url }) {
                     let commenterName = $itemName.text().split("\n")[1];
                     let $itemComment = $(item.children[3]);
                     let comment = $itemComment.text();
+                    //  Get the hebrew date, translate the month using the array above
+                    //  and concatenate to get full date
                     let commentHebrewDate = $($($($(item.children[1].children[1])[0].children[5])[0]).children()[0]).text().replace('.','').replace(',','').replace(' ','-').replace(' ', '-');
                     let commentHebrewMonth = commentHebrewDate.split('-')[1];
                     let monthNumber = hebrewMonths.indexOf(commentHebrewMonth) + 1;
@@ -40,6 +43,7 @@ function saveAllCommentsFromURL({ url }) {
                     let commentDate = commentHebrewDate.replace(commentHebrewMonth, monthNumber);
                     let commentTime = $($($($(item.children[1].children[1])[0].children[5])[0]).children()[2]).text() + ":00";
                     let commentDateTime = commentDate + 'T' + commentTime;
+                    //  Get the page url, the comment id and concatenate them
                     let pageUrl = $($('.title')[1].children[0]).attr('href');
                     let commentId = $(item).attr('id');
                     let commentUrl = pageUrl + '#' + commentId;
@@ -50,7 +54,6 @@ function saveAllCommentsFromURL({ url }) {
                             resolve();
                         })
                         .catch((err) => {
-                            console.log(err)
                             console.log('already exits!');
                             resolve();
                         });
--- src/invokers/scrape.js
+++ src/invokers/scrape.js
@@ -3,11 +3,7 @@ let db = require('../db/mongoClient');
 
 
 function scrapeHoops() {
-    var lim = 42;// parseInt(process.argv[2])
-    for(var i=10*lim;i<10*(lim+1);i++){
-        console.log(i);
-        let url = `http://www.hoops.co.il/?paged=` + i.toString();
-        scrapePage({ url: url });
-    }
+    let url = `http://www.hoops.co.il/?paged=` + i.toString();
+    scrapePage({ url: url });
 }
 module.exports.scrapeHoops = scrapeHoops;
